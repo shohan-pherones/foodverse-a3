083c52bc98a5c3a3fe5a727c0849ddc753c0a57a
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-20 22:51:30 +0600

    recipe item fetched

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
-const Home = ({ recipes }) => {
+import Recipe from "./Recipe";
+
+const Home = ({ recipes, loading, error }) => {
   return (
-    <div>
-      {recipes?.map((recipe) => (
-        <li key={recipe.recipe_id}>{recipe.title}</li>
-      ))}
+    <div className="home container mx-auto py-10 flex flex-wrap gap-10 justify-center">
+      {!loading && !error && recipes.length === 0 ? (
+        <p className="text-2xl lg:text-4xl font-semibold text-rose-300">
+          Nothing to show, please search something!
+        </p>
+      ) : null}
+
+      {loading && <p>{error ? error : "Loading..."}</p>}
+
+      {recipes.length > 0 &&
+        recipes.map((recipe) => (
+          <Recipe recipe={recipe} key={recipe.recipe_id} />
+        ))}
     </div>
   );
 };
